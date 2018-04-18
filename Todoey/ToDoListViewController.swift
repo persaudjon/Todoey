eee41dc1102684383018bf3d23f120a0f8bed65f
Jonathan Persaud <JonathanPersaud@jonathansmbp39.home>
2018-04-17 20:30:29 -0400

    -Add User Defaults(Core Data) -Add New task UIAlert with UItextField

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-let strangerItems = ["Take out Garbage","Kill Demogorgon","Buy Eggos"]
+var strangerItems = ["Take out Garbage","Kill Demogorgon","Buy Eggos"]
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.object(forKey: "strangerItemsArray") as? [String]{
+            print("Found Core Data For ToDo's")
+            strangerItems = items
+        }
         
     }
     //Creates amount of cells in table view
@@ -39,6 +44,29 @@ let strangerItems = ["Take out Garbage","Kill Demogorgon","Buy Eggos"]
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
     }
-
+    
+    //MARK - Add new ToDo Item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textfield = UITextField()
+        
+        let alertController = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add new Task", style: .default){(action) in
+            print(textfield.text!)
+            self.strangerItems.append(textfield.text!)
+            self.defaults.setValue(self.strangerItems, forKey: "strangerItemsArray")
+           self.tableView.reloadData()
+        }
+        
+            alertController.addTextField(configurationHandler: { (newTaskTextField) in
+                newTaskTextField.placeholder? = "Enter New Task"
+                textfield = newTaskTextField
+            })
+        
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
